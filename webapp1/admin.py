792d38bc9bf1e715145aa6b7d5c91d070ad6a6fa
baharak-ahmadipoor <88390314+baharak-ahmadipoor@users.noreply.github.com>
2022-10-10 11:54:50 +0330

    contact added to admin page

--- webapp1/admin.py
+++ webapp1/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import contact
+
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'email', 'created_date')
+    search_fields= ('name',)
+    list = ('created_date',)
+
+
+admin.site.register(contact, ContactAdmin)
