cfc9f3dbd759a91c0bcc264c75afc074b3093705
baharak-ahmadipoor <88390314+baharak-ahmadipoor@users.noreply.github.com>
2023-07-17 18:22:38 +0430

    pagination and search added
    search is enabled
    pagination is enabled

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from blog.views import *
 app_name = 'blog'
 urlpatterns = [
     path('index',blog_view, name='index'),
+    path('',blog_view, name='index'),
     path('<int:pid>',blog_single, name='single'),
     path('category/<str:cat_name>',blog_view, name='category'),
     path('author/<str:author_username>',blog_view, name='author'),
+    path('search/', blog_search, name='search'),
     path('test',test, name='test')
 #    path('<str:name>',test, name='test')
 ]
